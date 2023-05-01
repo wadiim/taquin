8bfb0462583f7b70b3e6321e8240dbd35c68096c
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2023-05-01 12:59:47 +0200

    Allow to specify max depth in DFSSolver.solve()

--- dfs.py
+++ dfs.py
@@ -6,24 +6,26 @@ from state import State
 
 class DFSSolver(Solver):
     @staticmethod
-    def solve(state: State, order: str) -> Optional[State]:
+    def solve(state: State, order: str, max_depth=32) -> Optional[State]:
         Solver.solve(state, order)
 
         goal = state.get_target_state()
         if state == goal:
             return state
 
-        stack = [state]
+        stack = [(state, 0)]
         visited = set()
 
         while len(stack) > 0:
-            node = stack.pop()
+            node, depth = stack.pop()
             if node not in visited:
                 visited.add(node)
+                if node == goal:
+                    return node
+                if depth >= max_depth:
+                    continue
                 for neighbour in reversed(node.get_neighbours(order)):
-                    if neighbour == goal:
-                        return neighbour
                     if neighbour not in visited:
-                        stack.append(neighbour)
+                        stack.append((neighbour, depth + 1))
 
         return None
